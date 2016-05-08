baa70a7bc6a9455e95b3f7bff63bf8c083532ba4
baffioso <baffyx@gmail.com>
2016-05-08 21:33:01 +0200

    aendret filnavn

--- sicilien/js/view.js
+++ sicilien/js/view.js
@@ -123,6 +123,7 @@ var mapView = {
 	    //add tilelayer
 	    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	        minZoom: 8,
 	        maxZoom: 18,
 	        id: 'baffioso.ie1ok8lg',
 	        accessToken: L.mapbox.accessToken
