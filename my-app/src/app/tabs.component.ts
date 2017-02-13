120f9c0073f1870babe553ea74e2524d45b3638d
baffioso <baffyx@gmail.com>
2017-02-13 19:02:28 +0100

    angular test

--- my-app/src/app/tabs.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-import {CarouselComponent} from './carousel.component';
-
-
-@Component({
-  selector: 'tabs',
-  templateUrl: './tabs.component.html',
-})
-export class TabsComponent {}
