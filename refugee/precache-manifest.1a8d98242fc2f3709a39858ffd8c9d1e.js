6217e73bf26f705d00eceb44377d7813344da532
baffioso <baffyx@gmail.com>
2019-12-22 23:19:27 +0100

    update

---
+++ refugee/precache-manifest.1a8d98242fc2f3709a39858ffd8c9d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b1c0072f532bf375f4aceced0500c2",
+    "url": "/refugee/index.html"
+  },
+  {
+    "revision": "889d4032b8fc2a81e191",
+    "url": "/refugee/static/css/main.18536f14.chunk.css"
+  },
+  {
+    "revision": "4a915acef3d332a8c26c",
+    "url": "/refugee/static/js/2.6b8886a1.chunk.js"
+  },
+  {
+    "revision": "889d4032b8fc2a81e191",
+    "url": "/refugee/static/js/main.7cc5bde8.chunk.js"
+  },
+  {
+    "revision": "d0ae4fe97cfe818700b6",
+    "url": "/refugee/static/js/runtime~main.b47857bb.js"
+  }
+]);
