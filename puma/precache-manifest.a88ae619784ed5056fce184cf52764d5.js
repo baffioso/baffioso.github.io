9a101722e762094daa6a27013a4d91f6ae1c6868
baffioso <baffyx@gmail.com>
2020-01-03 20:59:56 +0100

    init trippin

---
+++ puma/precache-manifest.a88ae619784ed5056fce184cf52764d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a7f75ec8e1b84be09eb36c4cb77e50",
+    "url": "/puma/index.html"
+  },
+  {
+    "revision": "18c96e6a710b1491e51d",
+    "url": "/puma/static/css/2.7a6f2ae5.chunk.css"
+  },
+  {
+    "revision": "ce3a19b5b4c7ef045f4c",
+    "url": "/puma/static/css/main.7e42e250.chunk.css"
+  },
+  {
+    "revision": "18c96e6a710b1491e51d",
+    "url": "/puma/static/js/2.ab5bbcb9.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/puma/static/js/2.ab5bbcb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce3a19b5b4c7ef045f4c",
+    "url": "/puma/static/js/main.a07c65cb.chunk.js"
+  },
+  {
+    "revision": "a4daba44a371dd5e5ae8",
+    "url": "/puma/static/js/runtime-main.593fdc6d.js"
+  }
+]);
--- puma/service-worker.js
+++ puma/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1da8abfce1e28a9ece2646baf8f11948.js"
+  "/puma/precache-manifest.a88ae619784ed5056fce184cf52764d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puma/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
